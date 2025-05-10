429bd955ad8733e2b7dd6df2fc748d371be0fee1
oussama <ossamaaiatameur63@gmail.com>
2025-05-10 20:55:37 +0200

    Uncomment and implement Dog class methods, add Family class with member management, and prepare TheIncredibles class for future use

--- week1/day4/Exercises/Exercise.py
+++ week1/day4/Exercises/Exercise.py
@@ -40,35 +40,86 @@
 # sara_pets.walk()
 
 
-# #############################2
-class Dog:
+# # #############################2
+# class Dog:
     
-    def __init__(self,name,age,weight):
-        self.name=name
-        self.age=age
-        self.weight=weight
+#     def __init__(self,name,age,weight):
+#         self.name=name
+#         self.age=age
+#         self.weight=weight
         
-    def bark(self):
-        return self.name
+#     def bark(self):
+#         return self.name
     
-    def run_speed(self):
-        return (self.weight)/(self.age*10)
+#     def run_speed(self):
+#         return (self.weight)/(self.age*10)
     
-    def fight(self,other_dog):
-        self_power=self.run_speed()*self.weight
-        other_power=other_dog.run_speed()*other_dog.weight 
+#     def fight(self,other_dog):
+#         self_power=self.run_speed()*self.weight
+#         other_power=other_dog.run_speed()*other_dog.weight 
 
-        if self_power > other_power:
-            return f"{self.name} wins the fight against {other_dog.name}!"
-        elif self_power < other_power:
-            return f"{other_dog.name} wins the fight against {self.name}!"
-        else:
-            return f"{self.name} and {other_dog.name} are equally strong."
+#         if self_power > other_power:
+#             return f"{self.name} wins the fight against {other_dog.name}!"
+#         elif self_power < other_power:
+#             return f"{other_dog.name} wins the fight against {self.name}!"
+#         else:
+#             return f"{self.name} and {other_dog.name} are equally strong."
         
 
-dog1=Dog("nex",10,20)
-dog2=Dog("frex",10,40)
-dog3=Dog("gambol",10,50)
+# dog1=Dog("nex",10,20)
+# dog2=Dog("frex",10,40)
+# dog3=Dog("gambol",10,50)
 
 
-###################################""3
+# ###################################""4
+class Family :
+    def __init__(self,last_name ):
+        self.last_name=last_name
+        self.members=[]
+
+    def born(self,**ne):
+        self.members.append(ne)
+        print("congratulating")
+
+    def is_18(self,name):
+        for member in self.members:
+            if  member["name"]==name:
+                return member["age"]>=18
+        return False
+    
+    def family_presentation(self):
+        print(f"{self.last_name}  family : ")
+        for x in self.members:
+            print(f"name : {x["name"]} age : {x["age"]} gender : {x["gender"]} is child : {x["child"]}")
+
+Fami=Family("ait ameur")
+
+
+# ###################################5
+
+# class TheIncredibles(Family):
+#     def __init__(self, last_name):
+#         super().__init__(last_name)
+
+#     def use_power(self,name):
+#         for x in self.members:
+#             if x['name'] == name:
+#                 if x["age"]>=18:
+#                     print(f"{x["power"]}")
+#                 else:
+#                     print("sorry")
+        
+#     def incredible_presentation(self):
+#         print("*Here is our powerful family **")
+#         super().family_presentation()
+
+# members = [
+#         {'name':'Michael', 'age':35, 'gender':'Male', 'is_child':False, 'power': 'fly', 'incredible_name':'MikeFly'},
+#         {'name':'Sarah', 'age':32, 'gender':'Female', 'is_child':False, 'power': 'read minds', 'incredible_name':'SuperWoman'}
+#     ]
+
+# fm=TheIncredibles("hosmos")
+# Fami.born(name="oussama",age=21,gender="male",child="False",power="fly",incredible_name="draga")
+# Fami.born(name="youssef",age=24,gender="male",child="False",power="read minds",incredible_name="hofazli9")
+# Fami.born(name="yahya",age=13,gender="male",child="True",power="sleep",incredible_name="tiwawa")
+# fm.incredible_presentation()
