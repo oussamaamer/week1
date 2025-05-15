4907bfd8b948e760f27f22abb16cc9f1a74e5d9e
oussama <ossamaaiatameur63@gmail.com>
2025-05-15 17:36:14 +0200

    Add SQL queries for retrieving items and customers data

---
+++ week2/day2/exercices1.sql
@@ -0,0 +1,4 @@
+SELECT * FROM items ORDER BY price
+SELECT * FROM items WHERE price>=80 ORDER BY price DESC
+SELECT nom,lastnom from customers  ORDER BY nom LIMIT 3 
+SELECT lastnom from customers ORDER BY lastnom DESC
