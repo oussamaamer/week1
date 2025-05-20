27dbcb248ac5f2c532d60fe964cf2338c5386775
oussama <ossamaaiatameur63@gmail.com>
2025-05-20 17:40:25 +0200

    Update exercise.html to reference daily1.js and add daily1.js with initial code

---
+++ week3/day2/daily1.js
@@ -0,0 +1,11 @@
+// let sentence = "The movie is not that bad, I like it";
+// let wordNot = sentence.indexOf("not");
+// let wordBad = sentence.indexOf("bad");
+// console.log(wordNot);
+// console.log(wordBad);
+// let t = sentence.split("");
+// if (wordNot < wordBad) {
+//   t.splice(wordNot, wordBad - wordNot + 3, "good");
+// }
+
+// console.log(t.join(""));
