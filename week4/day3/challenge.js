fa788e12cde50943ecdf8d97b8cfd7b794bc63b9
oussama <ossamaaiatameur63@gmail.com>
2025-06-04 14:27:14 +0200

    Comment out Video class and related code in challenge.js for clarity

--- week4/day3/challenge.js
+++ week4/day3/challenge.js
@@ -1,1 +1,33 @@
+// class Video {
+//   constructor(title, uploader, time) {
+//     this.title = title;
+//     this.uploader = uploader;
+//     this.time = time;
+//   }
 
+//   watch() {
+//     console.log(
+//       `${this.uploader} watched all ${this.time} seconds of "${this.title}"!`
+//     );
+//   }
+// }
+
+// const video1 = new Video("JavaScript", "oussama", 500);
+// video1.watch();
+
+// const video2 = new Video("flask", "ashraf", 1000);
+// video2.watch();
+
+// //bonus:
+// const videoData = [
+//   { title: "Intro to React", uploader: "noussayba", time: 450 },
+//   { title: "Python Basics", uploader: "mouad", time: 700 },
+//   { title: "CSS Flexbox Guide", uploader: "yahya", time: 400 },
+//   { title: "Node.js Tutorial", uploader: "youssef", time: 800 },
+//   { title: "Data Structures in JS", uploader: "driss", time: 500 },
+// ];
+
+// videoData.forEach((data) => {
+//   const video = new Video(data.title, data.uploader, data.time);
+//   video.watch();
+// });
